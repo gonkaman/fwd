375f9c0e41a407c697fb3d4cc202fc9d753d50b6
gonkaman <gonkaman225@live.fr>
2023-08-14 13:33:05 +0100

    +refactor api

--- result/js/src/index.ts
+++ result/js/src/index.ts
@@ -2,26 +2,27 @@ import { Result } from "./Result";
 import { FailureResult } from "./ResultImplFailure";
 import { SuccessResult } from "./ResultImplSuccess";
 
-export const result = {
-    /**
-     * Creates an instance of Result representing a success, using the given value
-     * @param value Result data for a success
-     * @returns An instance of Result
-     */
-    success<T,E>(value: T): Result<T,E>{
-        return new SuccessResult<T>(value) as Result<T,E>;
-    },
-    
-    /**
-     * Creates an instance of Result representing a failure, using the given error
-     * @param error Result data for a failure
-     * @returns An instance of Result
-     */
-    failure<T,E>(error: E): Result<T,E>{
-        return new FailureResult<E>(error) as Result<T,E>;
-    }
-};
-
 export type { Result }
 
+/**
+ * Creates an instance of Result representing a success, using the given value
+ * @param value Result data for a success
+ * @returns An instance of Result
+ */
+export const success = <T,E>(value: T): Result<T,E> => new SuccessResult<T>(value) as Result<T,E>;
+
+
+/**
+ * Creates an instance of Result representing a failure, using the given error
+ * @param error Result data for a failure
+ * @returns An instance of Result
+ */
+export const failure  = <T,E>(error: E): Result<T,E> => new FailureResult<E>(error) as Result<T,E>;
+
+
+export { 
+    Runner, PipeEntry, PipeBuilder, pipe, exec, map, swap, 
+    mapSuccess, swapSuccess, mapFailure, swapFailure,
+    fork, forkMap, forkSuccess, forkFailure 
+} from "./Pipe";
 
